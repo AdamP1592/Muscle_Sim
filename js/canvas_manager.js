53333165a76cd2ba9a7f2d273f38d0ddba09c148
Adam <safety5646@gmail.com>
2025-12-08 18:24:19 -0600

    Updated rendering to happen in the center rather than top left for everything.

--- js/canvas_manager.js
+++ js/canvas_manager.js
@@ -1,71 +1,78 @@
+//sets the graphing coordinate constraints
 const maxX = 100;
-const maxY = 100
+const maxY = 100;
+//pre creates variables for all the main parts of the sim
 var focused_button = null;
 var canvas = null;
 var ctx = null;
 var sim = null;
 var scalingFactor = null;
 
+//for ctrl + (key) events
 var controlPressed = false;
 
+//for client bounding rect
 var rect = null;
 
+//Storage for maintaining the connection between dt and fps
 var lastFrameTime = null;
 
+//resizeEventStuff
 var oldWidth = 0;
 var oldHeight = 0;
 
 function create_onclick_events(){
     let buttons = document.getElementsByClassName("spawn_button");
-    //console.log(buttons)
+    //create all spawn button events
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("mousedown", onclick_event)
     }
 }
+/**
+ * if a click is released, place the object that corresponds with the button that was pressed
+ * @param {*} event 
+ */
+function click_released(event){
+  
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  //scaled to between 0 to maxX/maxY 
+  let xScaled = maxX * (x / rect.width);
+  let yScaled = maxY * (y / rect.height);
+
+  //in bounds
+  if(!( x < 0 || y < 0 || x > rect.width || y > rect.height)){
+
+    switch(buttonID){
+      case "fixed_spawn":
+        sim.createFixedSquare(xScaled, yScaled)
+        break;
+      case "move_spawn":
+        sim.createMoveableSquare(xScaled, yScaled)
+        break;
+    }
+  }
+  else{
+    console.log("Out of bounds")
+  }
+  //reset event listeners and classes
+  let main = document.getElementById("main")
+  main.classList.remove('hover_with_obj');
+  document.removeEventListener('mouseup', click_released)
+} 
+/**
+ * A click event function for buttons 
+ * @param {*} event 
+ */
 function onclick_event(event){
-  //console.log("Clicked", event.srcElement)
   button = event.srcElement
   buttonID = event.srcElement.id
   document.addEventListener('mouseup', click_released);
 
   let main = document.getElementById("main");
   main.classList.add('hover_with_obj');
-  sim.createMuscle()
-  function click_released(event){
-    
-    //gets coords of rect
-
-    //console.log(`Event ${event}.\nRect: ${rect}`)
-
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-
-    //scaled to between 0 to maxX/maxY 
-    let xScaled = maxX * (x / rect.width);
-    let yScaled = maxY * (y / rect.height);
-    //console.log(`RawXY: ${x}, ${y} \nScaledXY(0 - maxY) ${xScaled}, ${yScaled}`)
-
-    //in bounds
-    if(!( x < 0 || y < 0 || x > rect.width || y > rect.height)){
-
-      switch(buttonID){
-        case "fixed_spawn":
-          //console.log("Button ID: ", buttonID);
-          sim.createFixedSquare(xScaled, yScaled)
-          break;
-        case "move_spawn":
-          sim.createMoveableSquare(xScaled, yScaled)
-          break;
-      }
-    }
-    else{
-      console.log("Out of bounds")
-    }
-    let main = document.getElementById("main")
-    main.classList.remove('hover_with_obj');
-    document.removeEventListener('mouseup', click_released)
-  } 
-
+  
 }
 
 function convertRawCoordsToCanvas(x, y){
@@ -85,11 +92,22 @@ function drawMuscles(){
     let [obj1X, obj1Y] = convertRawCoordsToCanvas(obj1.x, obj1.y);
     let [obj2X, obj2Y] = convertRawCoordsToCanvas(obj2.x, obj2.y);
 
+    let canvasWidth1 = obj1.width * scalingFactor
+    let canvasHeight1 = obj1.height * scalingFactor
+    let canvasWidth2 = obj2.width * scalingFactor
+    let canvasHeight2 = obj2.height * scalingFactor
+
+    obj1X -= canvasWidth1/2;
+    obj1Y -= canvasHeight1/2;
+
+    obj2X -= canvasWidth2/2;
+    obj2Y -= canvasHeight2/2;
+
     ctx.beginPath();
     ctx.strokeStyle = '#c21212ff';
     ctx.lineWidth = 3
-    ctx.moveTo(obj1X + (obj1.width * scalingFactor)/2, obj1Y + (obj1.height * scalingFactor)/2);
-    ctx.lineTo(obj2X + (obj2.width * scalingFactor)/2, obj2Y + (obj2.height * scalingFactor)/2);
+    ctx.moveTo(obj1X + (canvasWidth1)/2, obj1Y + (canvasHeight1)/2);
+    ctx.lineTo(obj2X + (canvasWidth2)/2, obj2Y + (canvasHeight2)/2);
 
     ctx.stroke();
 
@@ -109,18 +127,17 @@ function drawSquares(){
     let canvasHeight = obj.height * scalingFactor;
     
     let [xCanvas, yCanvas] = convertRawCoordsToCanvas(obj.x, obj.y)
-    //console.log("Scaled to fit current canvas:");
-    //console.log(`CanvasXY: (${yCanvas}, ${yCanvas})`)
 
-
-    ctx.fillRect(xCanvas, yCanvas, canvasWidth , canvasHeight);
+    let topCornerX = xCanvas - (canvasWidth/2);
+    let topCornerY = yCanvas - (canvasHeight/2);
+    ctx.fillRect(topCornerX, topCornerY, canvasWidth , canvasHeight);
     ctx.fillStyle = '#76acadff';
     if(obj.border){
-      ctx.strokeRect(xCanvas, yCanvas, canvasWidth, canvasHeight);
+      ctx.strokeRect(topCornerX, topCornerY, canvasWidth, canvasHeight);
     }
     let fontSize =  String(5 * scalingFactor);
     ctx.font = fontSize + 'px bold arial'
-    ctx.fillText(i, xCanvas + canvasWidth/2, yCanvas + canvasHeight/2)
+    ctx.fillText(i, xCanvas , yCanvas )
     
   }
   ctx.fill();
@@ -131,14 +148,13 @@ function draw(currentTime){
   const fps = 30
   const dt = 0.001
   let elapsedTime = currentTime - lastFrameTime;
-  //1000 ms per second
+  //dt is 1ms, there are 30 fps, elapsed time is 
   
   if (elapsedTime > 1000/fps){
     // in case fps is low, the sim adjusts each subsequent number
     // of steps to fit real-time simulation
     let stepCount = Math.round(elapsedTime / ( 1000 * dt ));
-    //let stepCount = 1;
-    //console.log(elapsedTime, stepCount)
+    // DEBUG: let stepCount = 1;
     //clear canvas
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -187,27 +203,28 @@ window.addEventListener("load", function() {
   resizeCanvas();
   requestAnimationFrame(draw);
 });
-
+/**
+ * sets up the first demo visualization of the simulation
+ */
 function demo1(){
-  sim.createFixedSquare(60, 60);
-  sim.createMoveableSquare(40, 40);
-  sim.createFixedSquare(60, 10)
-  sim.createFixedSquare(20, 40)
-  sim.createFixedSquare(60, 40)
+  //create objects
+  sim.createMoveableSquare(40, 40);//obj0
+  sim.createFixedSquare(60, 60); //obj1
+  sim.createFixedSquare(60, 20)//obj2
+  sim.createFixedSquare(20, 60)//obj3
+  sim.createFixedSquare(20, 20)//obj4
+  
+  //create muscles
+  sim.createMuscle(sim.objects[0], sim.objects[1], 0, 1)//m0
+  sim.createMuscle(sim.objects[0], sim.objects[2], 0, 2)//m1
+  sim.createMuscle(sim.objects[0], sim.objects[3], 0, 3)//m2
+  sim.createMuscle(sim.objects[0], sim.objects[4], 0, 4)//m3
 
-  sim.createMuscle(sim.objects[0], sim.objects[1], 0, 1)
-  //sim.createMuscle(sim.objects[1], sim.objects[2], 1, 2)
-  sim.createMuscle(sim.objects[1], sim.objects[2], 1, 2)
-  sim.createMuscle(sim.objects[1], sim.objects[3], 1, 3)
-  //sim.createMuscle(sim.objects[1], sim.objects[4], 1, 4)
 
-  console.log(sim.forceAddingElements)
-
-  sim.forceAddingElements[0].muscle.setStimulation(1, "sin", 1)
-  sim.forceAddingElements[1].muscle.setStimulation(0, "sin", 1)
-  //sim.forceAddingElements[1].muscle.setStimulation(0, "sin", 1)
+  sim.forceAddingElements[0].muscle.setStimulation(0, "sin", 1)
+  sim.forceAddingElements[1].muscle.setStimulation(0.25, "sin", 1)
   sim.forceAddingElements[2].muscle.setStimulation(0.5, "sin", 1)
-  //sim.forceAddingElements[3].muscle.setStimulation(0.5, "sin", 150)
+  sim.forceAddingElements[2].muscle.setStimulation(0.75, "sin", 1)
   
 }
 
@@ -225,7 +242,13 @@ function keyReleased(event){
   }
 }
 
-
+/**
+ * A click event used for muscle creation. 
+ * If control is pressed when clicking any subclass of rect, add that to the list of objects that will be getting a muscle.
+ * Once two objects are clicked, create a muscle.
+ * If you click a rect that has already been clicked, remove it from the list of objects.
+ * @param {*} event 
+ */
 function leftClickCanvas(event) {
   event.preventDefault();
 
@@ -270,6 +293,7 @@ function leftClickCanvas(event) {
       obj.border = false;
     }
   }
+  // if more than 1 object has a border create a muscle between the first two
   if(borderCount > 1){
     let objects = []
     for(const key in objectsWithBorders){
@@ -278,19 +302,12 @@ function leftClickCanvas(event) {
       objects.push([key, obj])
     }
     sim.createMuscle(objects[0][1], objects[1][1], objects[0][0], objects[1][0])
-    //console.log(sim.forceAddingElements);
   }
-
-
-}
-function sleep(miliseconds) {
-   var currentTime = new Date().getTime();
-
-   while (currentTime + miliseconds >= new Date().getTime()) {
-   }
 }
 
+//window events
 window.addEventListener("resize", resizeCanvas)
 
+//key events
 document.addEventListener("keydown", keyPressed);
 document.addEventListener("keyup", keyReleased);
